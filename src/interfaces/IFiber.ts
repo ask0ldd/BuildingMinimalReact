2833c06784b9c82f648ff316c6823e019ce6a8fc
Cedric B-Eff <cedrikopenclass@gmx.com>
2025-02-26 17:26:21 +0100

    *add* fiber interface

---
+++ src/interfaces/IFiber.ts
@@ -0,0 +1,12 @@
+export type IFiber = {
+    type: string
+    props: {
+        children: IFiber[]
+        [key: string]: unknown | never[] | string
+        nodeValue: string
+    }
+    child? : IFiber
+    parent: IFiber
+    dom: HTMLElement | Text | null
+    sibling? : IFiber
+}
